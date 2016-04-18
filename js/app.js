95712a8bb7d963394e16394aad2c67d76848e4c0
Nick Niles <nickyniles@gmail.com>
2016-04-18 16:22:40 +0200

    new states and styles

--- js/app.js
+++ js/app.js
@@ -3,25 +3,47 @@
 var futureDate = moment("20160507", "YYYYMMDD").fromNow();
 var provisionDate = moment(futureDate).subtract(90, "minutes");
 var now = moment().format('YYYY,MM,DD H:mm:ss');
-var in90min = moment().add(90, "minutes");
+var in90min = moment().add(60, "minutes");
 var till90 = moment(in90min).fromNow()
 var a = moment();
 var dur = a.to(in90min) // "in a day"
+var clockState = 0;
+var addFour = moment().add(4, "minutes");
+var in4Min = moment(now).add(4, "minutes").format('YYYY/MM/DD H:mm:ss');
+var in10Min = moment(now).add(10, "minutes").format('YYYY/MM/DD H:mm:ss');
 
 //set the 90 min clock
 $('.date-btn').on("click", function() {
-    $('#getting-started').text(dur);
-    $('#server-status-bar').addClass("provisionable");
-    $('#server-status-bar .status').html('<a href="#" class="button success">Launch Now!</a>');
+	if (clockState == 0) {
+		$('#time-till-event').text(dur);
+	    // $('#server-status-bar').addClass("provisionable");
+	    $('.status').addClass('hide');
+	    $('.launch-now').removeClass('hide')
+	} else if(clockState == 1){
+		$('.launching').addClass('hide')
+		$('.server').text('SERVER ONLINE')
+		$('#server-status-bar').addClass('online')
+	} else if(clockState == 2) {
+		$('#time-till-event').countdown(in10Min, function(event) {
+   			$(this).html(event.strftime('%-M:%S'));
+ 		});
+ 		$('.btns1 a.edit').addClass('hide')
+ 		$('.btns1 a.start').removeClass('hide')
+ 		$('table tr:nth-child(2)').addClass('next-talk')
+	}
 
-
+    clockState += 1;
 });
 
-$('#getting-started').text(futureDate)
+$('.launch-now').on('click', function(){
+    $('.launch-now').addClass('hide')
+    $('.launching').removeClass('hide')
+});
+
+$('#time-till-event').text(futureDate)
 
 $('#time-till-provisioning').text(futureDate)
 
-
-
-
-$(document).foundation();
+$('.launching .timer').countdown(in4Min, function(event) {
+   $(this).html(event.strftime('%-M:%S'));
+ });
