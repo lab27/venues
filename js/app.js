65ef3f8dbac859657fbedf20006cc2cbc692c2ea
Nick Niles <nickyniles@gmail.com>
2016-04-18 18:00:23 +0200

    more flow, playing animation, etc

--- js/app.js
+++ js/app.js
@@ -11,18 +11,31 @@ var clockState = 0;
 var addFour = moment().add(4, "minutes");
 var in4Min = moment(now).add(4, "minutes").format('YYYY/MM/DD H:mm:ss');
 var in10Min = moment(now).add(10, "minutes").format('YYYY/MM/DD H:mm:ss');
+var oneSecond = new Date().getTime() + 1000;
 
 //set the 90 min clock
 $('.date-btn').on("click", function() {
 	if (clockState == 0) {
 		$('#time-till-event').text(dur);
-	    // $('#server-status-bar').addClass("provisionable");
-	    $('.status').addClass('hide');
+	    $('.launchable-in').addClass('hide');
+	    $('.date-btn').addClass('hide');
 	    $('.launch-now').removeClass('hide')
+	    $('.launch-advice').removeClass('hide')
+
 	} else if(clockState == 1){
-		$('.launching').addClass('hide')
-		$('.server').text('SERVER ONLINE')
+		$('.launching-now').addClass('hide')
+		$('.server-status').text('SERVER ONLINE')
 		$('#server-status-bar').addClass('online')
+		$('.online-for').countdown(oneSecond, {elapse: true})
+
+		.on('update.countdown', function(event) {
+		  var $this = $(this);
+		  if (event.elapsed) {
+		    $this.html(event.strftime('<span>%H:%M:%S</span>'));
+		  } else {
+		    $this.html(event.strftime('<span>%H:%M:%S</span>'));
+		  }
+		});
 	} else if(clockState == 2) {
 		$('#time-till-event').countdown(in10Min, function(event) {
    			$(this).html(event.strftime('%-M:%S'));
@@ -30,6 +43,7 @@ $('.date-btn').on("click", function() {
  		$('.btns1 a.edit').addClass('hide')
  		$('.btns1 a.start').removeClass('hide')
  		$('table tr:nth-child(2)').addClass('next-talk')
+ 		$('.date-btn').addClass('hide');
 	}
 
     clockState += 1;
@@ -37,13 +51,54 @@ $('.date-btn').on("click", function() {
 
 $('.launch-now').on('click', function(){
     $('.launch-now').addClass('hide')
-    $('.launching').removeClass('hide')
+    $('.launch-advice').addClass('hide')
+    $('.launching-now').removeClass('hide')
+    $('#server-status-bar').addClass('provisioning')
+    $('.server-status').text('SERVER IS BEING LAUNCHED')
+    $('.date-btn').removeClass('hide');
+
 });
 
+//main countdown
 $('#time-till-event').text(futureDate)
 
+//server countdown
 $('#time-till-provisioning').text(futureDate)
 
-$('.launching .timer').countdown(in4Min, function(event) {
+//time till server is launched
+$('.launching-now .timer').countdown(in4Min, function(event) {
    $(this).html(event.strftime('%-M:%S'));
  });
+
+//Click Start Stream
+$('.start').on('click', function(){
+	$('.btns1 .stop').removeClass('hide');
+	$(this).addClass('hide');
+	$('table tr:nth-child(2) .live').removeClass('hide')
+	$('table tr:nth-child(2) .stopped').addClass('hide')
+	$('table tr:nth-child(2) td.soundbars-cell .soundbars').removeClass('hide');
+	$('table tr:nth-child(2) td.talk-time').countdown(oneSecond, {elapse: true})
+
+		.on('update.countdown', function(event) {
+		  var $this = $(this);
+		  if (event.elapsed) {
+		    $this.html(event.strftime('<span>%H:%M:%S</span>'));
+		  } else {
+		    $this.html(event.strftime('<span>%H:%M:%S</span>'));
+		  }
+		});
+});
+
+//CLICK STOP stream
+$('.stop').on('click', function(){
+	if (confirm('Are you sure you want to stop the stream?')) {
+    $('.btns1 .start').removeClass('hide');
+	$(this).addClass('hide');
+	$('table tr:nth-child(2) .stopped').removeClass('hide');
+	$('table tr:nth-child(2) .live').addClass('hide')
+	$('table tr:nth-child(2) td.soundbars-cell .soundbars').addClass('hide');
+} else {
+    // Do nothing!
+}
+
+});
