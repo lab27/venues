a53ca93b34ef53c8dd4f7fb8e317b4fa8ae0c839
Nick Niles <nickyniles@gmail.com>
2016-04-19 14:06:47 +0200

    major change to layout and logic, pretty good functioning demo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var stateMsg1 = "One hour before event.";
+var stateMsg2 = "~10 Minutes before event.";
+
+var insideTenMin = false;
+
 var futureDate = moment("20160507", "YYYYMMDD").fromNow();
 var provisionDate = moment(futureDate).subtract(90, "minutes");
 var now = moment().format('YYYY,MM,DD H:mm:ss');
@@ -11,24 +16,40 @@ var clockState = 0;
 var addFour = moment().add(4, "minutes");
 var in4Min = moment(now).add(4, "minutes").format('YYYY/MM/DD H:mm:ss');
 var in10Min = moment(now).add(10, "minutes").format('YYYY/MM/DD H:mm:ss');
+var inOneHour = moment(now).add(60, "minutes").format('YYYY/MM/DD H:mm:ss');
 var oneSecond = new Date().getTime() + 1000;
 
 //set the 90 min clock
 $('.date-btn').on("click", function() {
 	if (clockState == 0) {
-		$('#time-till-event').text(dur);
+		$('.clock-msg').countdown(inOneHour, function(event) {
+   			$(this).html(event.strftime('%-M:%S'));
+ 		});
+		$('.state-msg').text(stateMsg1);
 	    $('.launchable-in').addClass('hide');
 	    $('.date-btn').addClass('hide');
-	    $('.launch-now').removeClass('hide')
+	    $('.start-now').removeClass('hide')
 	    $('.launch-advice').removeClass('hide')
+	    $('.switch').removeClass('disabled')
+	    $('.server-label .badge').removeClass('secondary').addClass('warning').text('!')
 
 	} else if(clockState == 1){
 		$('.launching-now').addClass('hide')
-		$('.server-status').text('SERVER ONLINE')
-		$('#server-status-bar').addClass('online')
-		$('.online-for').countdown(oneSecond, {elapse: true})
+		// $('.server-status').text('SERVER ONLINE')
+		//$('#server-status-bar').addClass('online')
+ 	 	$('.switch').removeClass('starting')
+ 	 	$('.switch-active').text('Online')
+		
+		if ($('select').val() == 'vr') {
+			$('.clock-label .badge').removeClass('secondary').addClass('warning').text('!')
+ 	 		$('.main-clock .button').removeClass('disabled').addClass('success')
+		}
 
-		.on('update.countdown', function(event) {
+	
+ 	 	
+
+ 	 	$('.server-label .badge').removeClass('warning').addClass('success').html('&#x2713;')
+		$('.online-for').countdown(oneSecond, {elapse: true}).on('update.countdown', function(event) {
 		  var $this = $(this);
 		  if (event.elapsed) {
 		    $this.html(event.strftime('<span>%H:%M:%S</span>'));
@@ -37,30 +58,62 @@ $('.date-btn').on("click", function() {
 		  }
 		});
 	} else if(clockState == 2) {
-		$('#time-till-event').countdown(in10Min, function(event) {
+		$('.clock-msg').countdown(in10Min, function(event) {
    			$(this).html(event.strftime('%-M:%S'));
  		});
  		$('.btns1 a.edit').addClass('hide')
  		$('.btns1 a.start').removeClass('hide')
  		$('table tr:nth-child(2)').addClass('next-talk')
  		$('.date-btn').addClass('hide');
+		$('.state-msg').text(stateMsg2);
+
+		insideTenMin = true;
+		
+
+		if ($('select').val() == 'none') {
+			$('.device-label .badge').removeClass('warning').addClass('alert')
+		} else if ($('select').val() == 'vr') {
+			$('.soundbars').addClass('signal');
+			$('.start').removeClass('disabled')
+			$('.start').addClass('success')
+
+		}
+
 	}
 
     clockState += 1;
 });
 
-$('.launch-now').on('click', function(){
-    $('.launch-now').addClass('hide')
-    $('.launch-advice').addClass('hide')
-    $('.launching-now').removeClass('hide')
-    $('#server-status-bar').addClass('provisioning')
-    $('.server-status').text('SERVER IS BEING LAUNCHED')
-    $('.date-btn').removeClass('hide');
-
+// select change
+$('select').change(function(){
+  if($(this).val() == 'vr'){ 
+    $('.device-label .badge').removeClass('warning').removeClass('alert').addClass('success').html('&#x2713;')
+  } else if ($(this).val() == 'none') {
+  	 $('.device-label .badge').removeClass('success').removeClass('alert').addClass('warning').html('!')
+  } else if ($(this).val() == 'new') {
+  	$('#device-modal').foundation('open');
+  }
+  if (insideTenMin == true) {
+  		$('.soundbars').addClass('signal');
+		$('.start').removeClass('disabled')
+  }
 });
 
+
+// Flip server switch
+$('.switch-input').change(function() {
+    if(this.checked) {
+    $('.start-now').addClass('hide')
+    $('.launch-advice').addClass('hide')
+    $('.launching-now').removeClass('hide')
+    $('.date-btn').removeClass('hide');
+    }
+});
+
+
+
 //main countdown
-$('#time-till-event').text(futureDate)
+$('.clock-msg').text(futureDate)
 
 //server countdown
 $('#time-till-provisioning').text(futureDate)
@@ -72,14 +125,14 @@ $('.launching-now .timer').countdown(in4Min, function(event) {
 
 //Click Start Stream
 $('.start').on('click', function(){
-	$('.btns1 .stop').removeClass('hide');
+	$('.stop').removeClass('hide');
 	$(this).addClass('hide');
+	$('.venue-title .label').removeClass('hide')
+	$('.clock-label .badge').removeClass('warning').addClass('success').html('&#x2713;')
 	$('table tr:nth-child(2) .live').removeClass('hide')
 	$('table tr:nth-child(2) .stopped').addClass('hide')
-	$('table tr:nth-child(2) td.soundbars-cell .soundbars').removeClass('hide');
-	$('table tr:nth-child(2) td.talk-time').countdown(oneSecond, {elapse: true})
-
-		.on('update.countdown', function(event) {
+	//$('table tr:nth-child(2)').removeClass('next-talk').addClass('streaming')
+	$('table tr:nth-child(2) td.talk-time').countdown(oneSecond, {elapse: true}).on('update.countdown', function(event) {
 		  var $this = $(this);
 		  if (event.elapsed) {
 		    $this.html(event.strftime('<span>%H:%M:%S</span>'));
@@ -92,11 +145,15 @@ $('.start').on('click', function(){
 //CLICK STOP stream
 $('.stop').on('click', function(){
 	if (confirm('Are you sure you want to stop the stream?')) {
-    $('.btns1 .start').removeClass('hide');
+    $('.start').removeClass('hide');
 	$(this).addClass('hide');
+	$('.venue-title .label').addClass('hide')
+	$('.clock-label .badge').removeClass('success').addClass('alert').html('!')
 	$('table tr:nth-child(2) .stopped').removeClass('hide');
 	$('table tr:nth-child(2) .live').addClass('hide')
-	$('table tr:nth-child(2) td.soundbars-cell .soundbars').addClass('hide');
+	$('.soundbars').removeClass('signal');
+	//$('table tr:nth-child(2)').removeClass('streaming').addClass('interrupted')
+
 } else {
     // Do nothing!
 }
